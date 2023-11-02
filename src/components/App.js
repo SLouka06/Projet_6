f2cfcda5a0dd6afea4a3417d6bf0c2e271b76acc
Louis Stambolic <louis.stambolic@outlook.fr>
2023-11-02 20:26:35 +0100

    Ajustement de react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,18 @@
 import '../styles/App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div>
-        <Switch>
-          <Route path="/about">
-            {/* Votre composant pour la page À propos */}
-          </Route>
-          <Route path="/users">
-            {/* Votre composant pour la page Utilisateurs */}
-          </Route>
-          <Route path="/">
-            {/* Votre composant pour la page d'accueil */}
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/about" element={<VotreComposantAbout />} />
+          <Route path="/users" element={<VotreComposantUsers />} />
+          <Route path="/" element={<VotreComposantAccueil />} />
+        </Routes>
       </div>
     </Router>
   );
 }
 
-export default App;
+export default App
